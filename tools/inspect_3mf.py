40589b78e407b3e9f566285d507940effc0711c9
WillJohns0n <willj82@gmail.com>
2026-01-08 21:45:59 +0000

    chore: ignore tools/, backup and exported files

--- tools/inspect_3mf.py
+++
@@ -1,7 +0,0 @@
-import zipfile, sys
-p = sys.argv[1]
-with zipfile.ZipFile(p,'r') as z:
-    print('\n'.join(z.namelist()))
-    data = z.read('3D/3dmodel.model')
-    print('\n--- model start ---\n')
-    print(data.decode('utf-8')[:2000])
